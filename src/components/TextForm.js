2f1c80910ef7089264c6188fc392a815bcf79d06
Jainendra Pal <jainendra161093@gmail.com>
2023-03-26 20:55:38 +0530

    latest changes added

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import "./../css/Textform.css";
 
 export default function TextForm(props) {
 
@@ -58,6 +59,38 @@ props.showAlert('Converted to lowercase!',"success");
   return (
     <>
     <div className="container">
+
+    {/* <!-- Content section--> */}
+        <section className="py-5">
+            <div className="container my-5">
+                <div className="row justify-content-center">
+                    <div className="col-lg-6">
+                        <h2>Full Width Backgrounds</h2>
+                        <p className="lead">A single, lightweight helper class allows you to add engaging, full width background images to sections of your page.</p>
+                        <p className="mb-0">The universe is almost 14 billion years old, and, wow! Life had no problem starting here on Earth! I think it would be inexcusably egocentric of us to suggest that we're alone in the universe.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        {/* <!-- Image element - set the background image for the header in the line below--> */}
+        <div className="py-5 bg-image-full bkImage">
+            {/* <!-- Put anything you want here! The spacer below with inline CSS is just for demo purposes!--> */}
+            <div className="gap-div"></div>
+        </div>
+        {/* <!-- Content section--> */}
+        <section className="py-5">
+            <div className="container my-5">
+                <div className="row justify-content-center">
+                    <div className="col-lg-6">
+                        <h2>Engaging Background Images</h2>
+                        <p className="lead">The background images used in this template are sourced from Unsplash and are open source and free to use.</p>
+                        <p className="mb-0">I can't tell you how many people say they were turned off from science because of a science teacher that completely sucked out all the inspiration and enthusiasm they had for the course.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
         <h1 style={{color: props.mode === 'dark' ? 'white': 'black'}}>{props.heading}</h1>
        <div className="mb-3">
       <textarea
@@ -84,6 +117,10 @@ props.showAlert('Converted to lowercase!',"success");
       <h3>Preview</h3>
       <p>{text.length>0 ? text: "Enter your text to preview"}</p>
     </div>
+
+    {/* <footer className="py-5 bg-light">
+            <div className="container"><p className="m-0 text-center text-white">Copyright &copy; Jai Trading Comapny 2023</p></div>
+        </footer> */}
     </>
   );
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,37 +1,68 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
+import "../../css/Header.css";
 
 export default function Header(props) {
   return (
     <div>
-       <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">{props.title}</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        {/* <li className="nav-item">
+      <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+        <div className="container-fluid">
+          <a className="navbar-brand headerText" href="/">
+            {props.title}
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              {/* <li className="nav-item">
           <a className="nav-link" href="/">Link</a>
         </li> */}
-        
-       </ul>
-       <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark': 'light'}`}>
-  <input className="form-check-input" type="checkbox" onClick={props.toggleMode} role="switch" id="flexSwitchCheckDefault"/>
-  <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
-</div>
+            </ul>
+            <div
+              className={`form-check form-switch text-${
+                props.mode === "light" ? "dark" : "light"
+              }`}
+            >
+              <input
+                className="form-check-input"
+                type="checkbox"
+                onClick={props.toggleMode}
+                role="switch"
+                id="flexSwitchCheckDefault"
+              />
+              <label
+                className="form-check-label"
+                htmlFor="flexSwitchCheckDefault"
+              >
+                Enable Dark Mode
+              </label>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <header className="py-5 bg-image-full headerTag" >
+            <div className="text-center my-5">
+                <img className="img-fluid rounded-circle mb-4" src="https://dummyimage.com/150x150/6c757d/dee2e6.jpg" alt="..." />
+                <h1 className="text-white fs-3 fw-bolder">Full Width Pics</h1>
+                <p className="text-white-50 mb-0">Landing Page Template</p>
+            </div>
+        </header>
     </div>
-  </div>
-</nav>
-    </div>
-  )
+  );
 }
 
-Header.propTypes = {title: PropTypes.string,
-                   about:PropTypes.string
-}
-
+Header.propTypes = { title: PropTypes.string, about: PropTypes.string };
